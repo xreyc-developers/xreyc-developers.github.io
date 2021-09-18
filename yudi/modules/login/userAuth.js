02c92e8a6fe69b419ce87cde10e8f0a27dd0221b
xreyc <segumareyco@gmail.com>
2021-09-18 22:01:05 +0800

    cleaner code

--- yudi/modules/login/userAuth.js
+++ yudi/modules/login/userAuth.js
@@ -8,41 +8,38 @@ const url = window.location.origin + "/yudi/";
 const popup = document.querySelector('div.popup-container');
 
 /* LOGIN */
-export function loginUser(id) {
-  popup.style.display = "block";
-  fetch('./modules/login/users.json')
-    .then((data) => {
-      return data.json();
-    })
-    .then((users) => {
-      const email = users[id].email;
-      const password = users[id].password;
-      // LOGIN USER
-      signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          const loggedUser = userCredential.user;
-          localStorage.setItem('uid', loggedUser.uid);
-          localStorage.setItem('accessToken', loggedUser.accessToken);
-          localStorage.setItem('refreshToken', loggedUser.stsTokenManager.refreshToken);
-          localStorage.setItem('loggedIn', new Date());
-          setTimeout(() => {
-            window.location.replace(url + "pages/projects/");
-          },2000)
-
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    })  // LOGIN USER
-    .catch((err) => {
-      console.log("Something went wrong.");
-    })
+export async function loginUser(id) {
+  try{
+    popup.style.display = "block";
+    // GET USER FROM JSON
+    const rawUsers =  await fetch('./modules/login/users.json');
+    const users = await rawUsers.json();
+    const email = users[id].email;
+    const password = users[id].password;
+    // AUTHENTICATE USER
+    const authResponse = await signInWithEmailAndPassword(auth, email, password);
+    const loggedUser = authResponse.user;
+    localStorage.setItem('uid', loggedUser.uid);
+    localStorage.setItem('accessToken', loggedUser.accessToken);
+    localStorage.setItem('refreshToken', loggedUser.stsTokenManager.refreshToken);
+    localStorage.setItem('loggedIn', new Date());
+    setTimeout(() => {
+      popup.style.display = "none";
+      window.location.replace(url + "pages/projects/");
+    },2000)
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 /* LOGOUT */
 export function logoutUser() {
-  localStorage.removeItem('uid');
-  localStorage.removeItem('accessToken');
-  localStorage.removeItem('refreshToken');
-  localStorage.removeItem('loggedIn');
+  try{
+    localStorage.removeItem('uid');
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('loggedIn');
+  } catch(err) {
+    console.log(err);
+  }
 }
