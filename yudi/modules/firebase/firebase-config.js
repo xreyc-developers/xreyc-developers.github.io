81b491a0094262fa97b20bb083d04c4e138ba0b6
xreyc <segumareyco@gmail.com>
2021-09-18 13:12:08 +0800

    firebase

---
+++ yudi/modules/firebase/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBjvp2PwVDpm0ebUhfcirpZLR91iXuHYow",
+  authDomain: "javascript-yudi.firebaseapp.com",
+  projectId: "javascript-yudi",
+  storageBucket: "javascript-yudi.appspot.com",
+  messagingSenderId: "817315542839",
+  appId: "1:817315542839:web:cad2409f4693982357ccb2",
+  measurementId: "G-BB2W24QHWN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
