61c35f41dd16488025142607e333d726d207ead1
xreyc <segumareyco@gmail.com>
2021-09-18 16:06:30 +0800

    authentication

--- yudi/main.js
+++ yudi/main.js
@@ -1,16 +1,18 @@
-import { login } from './modules/login/login.js';
+import { loginUser } from './modules/login/loginUser.js';
 
 const APP = (function() {
   document.addEventListener('DOMContentLoaded',init);
+  // INITIALIZE APPLICATION
   function init() {
     addEventListeners();
   }
+  // ADD EVENT LISTENERS
   function addEventListeners() {
     const selectBtn = document.querySelectorAll('button');
     for(let i = 0; i < selectBtn.length; i++) {
       selectBtn[i].addEventListener('click', (e) => {
-        e.preventDefault()
-        login(i);
+        e.preventDefault();
+        loginUser(i);
       })
     }
   }
--- yudi/modules/firebase/firebase-config.js
+++ yudi/modules/firebase/firebase-config.js
@@ -1,12 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
-//import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBjvp2PwVDpm0ebUhfcirpZLR91iXuHYow",
   authDomain: "javascript-yudi.firebaseapp.com",
   projectId: "javascript-yudi",
@@ -15,7 +7,3 @@ const firebaseConfig = {
   appId: "1:817315542839:web:cad2409f4693982357ccb2",
   measurementId: "G-BB2W24QHWN"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
