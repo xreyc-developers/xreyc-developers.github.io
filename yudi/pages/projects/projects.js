e6531d83b149c0a78fe8bd9492ea637fae8ca776
xreyc <segumareyco@gmail.com>
2021-09-19 20:47:15 +0800

    projects

--- yudi/pages/projects/projects.js
+++ yudi/pages/projects/projects.js
@@ -19,6 +19,7 @@ const APP = (function() {
   function addEventListeners() {}
   // GET DATA
   async function getInitialData() {
+
     const projectContentRows = document.querySelector("#projectContentRows");
     const data = await getAllProjects();
     let rows = '';
@@ -27,11 +28,15 @@ const APP = (function() {
       const createdOn = new Date(rowData.createdOn.toDate());
       const updatedOn = new Date(rowData.updatedOn.toDate());
       rows += `
-        <div class="col-md-3">${rowData.name}</div>
-        <div class="col-md-3">${getMonthByIndex(createdOn.getMonth())} ${createdOn.getDate()}, ${createdOn.getFullYear()}</div>
-        <div class="col-md-3">${getMonthByIndex(updatedOn.getMonth())} ${updatedOn.getDate()}, ${updatedOn.getFullYear()}</div>
-        <div class="col-md-3">${rowData.status}</div>
-      `;
+        <div class="row yudi-table yudi-table-row" id="${doc.id}">
+          <div class="col-md-3">${rowData.name}</div>
+          <div class="col-md-3">${getMonthByIndex(createdOn.getMonth())} ${createdOn.getDate()}, ${createdOn.getFullYear()}</div>
+          <div class="col-md-3">${getMonthByIndex(updatedOn.getMonth())} ${updatedOn.getDate()}, ${updatedOn.getFullYear()}</div>
+          <div class="col-md-3">
+            ${rowData.status}
+            <i class="fas fa-ellipsis-h yudi-table-menu"></i>
+          </div>
+        </div>`;
     });
     projectContentRows.innerHTML = rows;
   }
