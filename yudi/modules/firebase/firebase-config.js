889af032b962690fd8b8d961fba804db64ffa45c
xreyc <segumareyco@gmail.com>
2024-04-10 23:18:43 +0800

    new update

--- yudi/modules/firebase/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBjvp2PwVDpm0ebUhfcirpZLR91iXuHYow",
-  authDomain: "javascript-yudi.firebaseapp.com",
-  projectId: "javascript-yudi",
-  storageBucket: "javascript-yudi.appspot.com",
-  messagingSenderId: "817315542839",
-  appId: "1:817315542839:web:cad2409f4693982357ccb2",
-  measurementId: "G-BB2W24QHWN"
-};
